588856856d7acfa524fab6c8c028792af17d63d6
asmaa <asmaabenhaili1@gmail.com>
2024-06-03 00:29:55 +0100

    security

---
+++ src/app/guards/authorization.guard.ts
@@ -0,0 +1,30 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import {AuthService} from "../services/auth.service";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean|UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.roles.includes("ADMIN")){
+      return true
+    }else {
+      this.router.navigateByUrl("admin/notAuthorized")
+      return false
+    }
+  }
+}
